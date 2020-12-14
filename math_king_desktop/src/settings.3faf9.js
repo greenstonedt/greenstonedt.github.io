badfa356825c65d3e7702c54a13a69c81428e64c
Thạch Trương <greenstonedt@gmail.com>
2020-12-14 12:38:03 +0700

    Updated MathKing

---
+++ math_king_desktop/src/settings.3faf9.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/landing.fire",orientation:"",jsList:[],bundleVers:{internal:"42dd2",resources:"905e4",main:"bff21"}};
