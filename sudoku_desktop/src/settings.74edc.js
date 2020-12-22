c06a09b57f73adda37fbb32dbc46ac8a014eccb1
Thạch Trương <greenstonedt@gmail.com>
2020-12-22 18:43:09 +0700

    Added sudoku

---
+++ sudoku_desktop/src/settings.74edc.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/landing.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "ce4d0",
+        main: "54e2f"
+    }
+};
