b31b3284e30c161857e11e9b16ef19221f000f27
Thạch Trương <greenstonedt@gmail.com>
2020-12-30 13:49:28 +0700

    updated sudoku ui

---
+++ sudoku_desktop/src/settings.f6083.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/landing.fire",orientation:"",jsList:[],bundleVers:{internal:"fbb78",resources:"1216d",main:"063d5"}};
