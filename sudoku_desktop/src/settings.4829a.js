dda2d099c1a1166566f674451303fa90a470074e
Thạch Trương <greenstonedt@gmail.com>
2020-12-31 14:43:15 +0700

    Updated sudoku

--- sudoku_desktop/src/settings.4829a.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/landing.fire",orientation:"",jsList:[],bundleVers:{internal:"fbb78",resources:"1216d",main:"c76d1"}};
