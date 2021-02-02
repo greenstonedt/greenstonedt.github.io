5917409aaaf83bd5bca323a69bf1fa47273a72a2
Thạch Trương <greenstonedt@gmail.com>
2021-02-02 12:14:29 +0700

    Update

---
+++ drop_desktop/src/settings.eb376.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/landing.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"fbb78",resources:"78230",main:"5e641"}};
